ac3ed46a5e1b75ea260f3c919ada0387a333f514
sonu <sonusaikishan@gmail.com>
2018-01-25 22:29:26 +0530

    ...

--- microservices/api/src/config.js
+++ microservices/api/src/config.js
@@ -4,11 +4,6 @@ let projectConfig = {
     }
 }
 
-function getSavedToken()
-{
-  return window.localStorage.getItem('authToken');
-}
-
 if (process.env.ENVIRONMENT === 'dev') {
   projectConfig.url.data = 'http://127.0.0.1:6432/v1/query';
 }
