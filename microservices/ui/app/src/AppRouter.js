c50b7019bc896c389ac52d952f7122370723f47b
sonu <sonusaikishan@gmail.com>
2018-01-25 19:39:56 +0530

    ...

--- microservices/ui/app/src/AppRouter.js
+++ microservices/ui/app/src/AppRouter.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import { Data } from './Data';
 import { Auth } from './Auth';
 import { Filestore } from './Filestore';
+import GetCredentials from './GetCredentials';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class AppRouter extends React.Component {
@@ -22,6 +23,7 @@ class AppRouter extends React.Component {
             <Route exact path="/auth" component={Auth}/>
             <Route exact path="/filestore" component={Filestore}/>
             <Route exact path="/home" component={Home} />
+            <Route exact path="/get-credentials" component={GetCredentials} />
           </div>
         </Router>
       </MuiThemeProvider>
--- microservices/ui/app/src/Home.js
+++ microservices/ui/app/src/Home.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-
+import { getSavedToken } from './config';
 import 'antd/dist/antd.css';
-
+const authToken = getSavedToken();
 class Home extends Component {
+    getData = () => {
+
+
+                console.log('CLicked getdata');
+
+                axios({
+                    method: 'post',
+                    url: 'https://api.artfully11.hasura-app.io/data',                                           //URL to be modified here
+                    data: { auth: authToken },
+                    config: { headers: { 'Content-Type': 'application/json' } }
+                })
+                    .then(function (response) {
+                        console.log('Successful post request');
+                        console.log(response);
+                        //TODO: Display credentials got from response in a copiable span
+
+                    })
+                    .catch(function (response) {
+                        console.log('Unsuccessful post request');
+                        console.log(response);
+                        alert('Sorry, Server Error!');
+
+                    });
+
+
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Home Page</h1>
                 <div>
                     <Button type="primary" href="/get-credentials">Get Credentials</Button>
-
+                    <Button type="primary" onClick={this.getData}>Data</Button>
                 </div>
             </div>
         );
