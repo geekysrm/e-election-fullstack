891a15ea4e2d2698e8ffe2434c0b4b66678276a5
sonu <sonusaikishan@gmail.com>
2018-01-31 02:36:54 +0530

    .

--- microservices/api/src/index.js
+++ microservices/api/src/index.js
@@ -20,7 +20,7 @@ details.onclick = function()
       }
     }
   };
-  request.open('POST','/can-vote',true);
+  request.open('POST','/can-nominate',true);
   request.setRequestHeader('Content-Type','application/json');
   request.send(null);
 }
