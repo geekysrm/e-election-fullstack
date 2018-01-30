5ab2ac7d63852a8af3b764eb196a7405ebf84d00
sonu <sonusaikishan@gmail.com>
2018-01-30 18:43:37 +0530

    .

--- microservices/ui/app/src/elections.js
+++ microservices/ui/app/src/elections.js
@@ -8,40 +8,45 @@ class elections extends Component
 {
 
   loadCards = () =>{
+
     axios({
+        method: 'post',
+        url: 'https://api.artfully11.hasura-app.io/get-elections',
+        config: { headers: { 'Content-Type': 'application/json' } }
+    })
+        .then(function (response) {
+            console.log('Successful post request');
+            console.log(response);
+        })
+        .catch(function (response) {
+          console.log('UnSuccessful post request');
+          console.log(response);
+        });
+
+{/*    axios({
         method: 'post',
         url: 'https://api.artfully11.hasura-app.io/get-elections',                                           //URL to be modified here
         config: { headers: { 'Content-Type': 'application/json' } }
     })
-        .then(function (response) {
-          {/*var card = ``;
+        .then(function (response) {console.log("hello");
           for(var i=0;i<response.data.length;i++)
           {
-
-              card = card +`<Card title=response.data[i].id extra={<a href="#">More</a>} style={{ width: 300 }}>
+              <Card title=response.data[i].id extra={<a href="#">More</a>} style={{ width: 300 }}>
                 <p>{response.data[i].post}</p>
                 <p>{response.data[i].state}</p>
                 <p>{response.data[i].nomination_start_time}</p>
                 <p>{response.data[i].nomination_end_time}</p>
                 <p>{response.data[i].election_start_time}</p>
                 <p>{response.data[i].election_end_time}</p>
-              </Card>`
-
+              </Card>
           }
-          return card;*/}
-          return (<Card title={response.data[0].id} extra={<a href="#">More</a>} style={{ width: 300 }}>
-            <p>{response.data[0].post}</p>
-            <p>{response.data[0].state}</p>
-            <p>{response.data[0].nomination_start_time}</p>
-            <p>{response.data[0].nomination_end_time}</p>
-            <p>{response.data[0].election_start_time}</p>
-            <p>{response.data[0].election_end_time}</p>
-          </Card>)
           console.log(response);
+          //return(<h1>hello</h1>);
         })
-        .catch(function (response) {
+        .catch(function (response) {console.log("helloc");
           console.log(response);
-        });
+          //return(<h1>hello</h1>);
+        });*/}
   }
   constructor()
   {
