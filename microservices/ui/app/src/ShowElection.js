34ff1733ffbfc752e342589b8dd31af18d657255
Soumya <soumyacool2012@gmail.com>
2018-02-01 02:52:23 +0530

    Hello

--- microservices/ui/app/src/ShowElection.js
+++ microservices/ui/app/src/ShowElection.js
@@ -2,17 +2,50 @@ import React from 'react';
 import { Button } from 'antd';
 import 'antd/dist/antd.css';
 
-class ShowElection extends React.Component { 
-
-
+// const ShowElection = ( { match: { params: {id } } } ) => (
+//     <h1>{id}</h1>
     
-render( { match: { params: {id } } } ) 
-{    return (
-        <h1>{id}</h1>
-        
-    );
+// );
+
+class ShowElection extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // this.state = {
+        // };
+    }
+
+    // componentDidMount() {
+    //     axios({
+    //         method: 'post',
+    //         url: 'https://api.artfully11.hasura-app.io/get-elections',
+
+    //         config: { headers: { 'Content-Type': 'application/json' } }
+    //     })
+    //         .then(response => {
+    //             console.log(response.data);
+
+
+
+
+    //             this.setState({ elections: response.data });
+    //         })
+    //         .catch(error => {
+    //             alert(`Sorry, can't fetch elections right now!`);
+    //             console.log('Post request failed!');
+    //         });
+    // }
+
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.match.id}</h1>
+
+            </div>
+        );
+    }
 }
 
-}
-    
 export default ShowElection;
