036306b8ba41aa58f5d97da8afd53602be369278
sonu <sonusaikishan@gmail.com>
2018-02-01 00:50:09 +0530

    ..

--- microservices/ui/app/src/Auth.js
+++ microservices/ui/app/src/Auth.js
@@ -1,46 +1,30 @@
+
 import React from 'react';
 import axios from 'axios';
-import TextField from 'material-ui/TextField';
-import Paper from 'material-ui/Paper';
-import FlatButton from 'material-ui/FlatButton';
-import Dialog from 'material-ui/Dialog';
-import Divider from 'material-ui/Divider';
-import Subheader from 'material-ui/Subheader';
-import LinearProgress from 'material-ui/LinearProgress';
-import {Card, CardText} from 'material-ui/Card';
+import 'antd/dist/antd.css';
+import './Auth.css';
+import { Form, Icon, Input, Button } from 'antd';
+
 import { saveOffline, getSavedToken } from './config';
 import { authenticateUser } from './api';
 
+const FormItem = Form.Item;
 
-const textfieldStyle = {
-  width: 300
-}
-
-class Auth extends React.Component {
-
-  constructor() {
-    super()
-    this.state = {
-      username: '',
-      password: '',
-      showLoadingIndicator: false,
-      showAlert: false,
-      alertMessage: ''
-    };
-  }
-
-  showProgressIndicator = (shouldShow) => {
-    this.setState({
-      ...this.state,
-      showLoadingIndicator: shouldShow
+class AuthForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
     });
   }
 
   login = () => {
     console.log('on login clicked');
-    this.showProgressIndicator(true)
+    //this.showProgressIndicator(true)
     authenticateUser(this.state.username, this.state.password, false).then(authResponse => {
-      this.showProgressIndicator(false)
+      //this.showProgressIndicator(false)
       console.log(authResponse);
       if (authResponse.auth_token) {
         //Save the auth token offline to be used by the filestore service
@@ -56,9 +40,9 @@ class Auth extends React.Component {
 
   register = () => {
     console.log('on register clicked');
-    this.showProgressIndicator(true)
+    //this.showProgressIndicator(true)
     authenticateUser(this.state.username, this.state.password, true).then(authResponse => {
-      this.showProgressIndicator(false)
+      //this.showProgressIndicator(false)
       console.log(authResponse);
       if (authResponse.auth_token) {
         saveOffline(authResponse.auth_token)
@@ -72,40 +56,11 @@ class Auth extends React.Component {
     });
   }
 
-  handleUsernameChange = (e) => {
-    this.setState({
-      ...this.state,
-      username: e.target.value
-    });
-  }
-
-  handlePasswordChange = (e) => {
-    this.setState({
-      ...this.state,
-      password: e.target.value
-    });
-  }
-
-  closeAlert = () => {
-    this.setState({
-      ...this.state,
-      showAlert: false,
-      alertMessage: ''
-    });
-  };
-
-  showAlert = (message) => {
-    this.setState({
-      ...this.state,
-      showAlert: true,
-      alertMessage: message
-    });
-  }
-
-
   render() {
+    const { getFieldDecorator } = this.props.form;
+
     const styleObj = {
-      height: '350px',
+      height: '250px',
       width: '500px',
       margin: '20px',
       textAlign: 'center',
@@ -113,68 +68,49 @@ class Auth extends React.Component {
       position: 'fixed',
       top: '50%',
       left: '50%',
-      transform: 'translate(-50%,-50%)'
-    };
-
-    const buttonContainerStyle = {
-      position: 'absolute',
-      bottom: '20px',
-      width: '100%',
-      textAlign: 'center'
+      transform: 'translate(-50%,-50%)',
+      padding: "24px",
+      background: "#fbfbfb",
+      border: "1px solid #d9d9d9",
+      borderRadius: "6px",
     };
 
     return (
-      <div>
-        <Paper
-          style={styleObj}
-          zDepth={1}>
-          {this.state.showLoadingIndicator ? <LinearProgress mode="indeterminate" /> : null}
-          <br /><br />
-          <TextField
-            onChange={this.handleUsernameChange}
-            floatingLabelText="Username"
-            style={textfieldStyle}
-            hintText="Username"/>
-
-          <TextField
-            onChange={this.handlePasswordChange}
-            floatingLabelText="Password"
-            style={textfieldStyle}
-            type="password"
-            hintText="password"/>
-
-          <div style={buttonContainerStyle}>
-            <FlatButton
-              label="Login"
-              primary={true}
-              onClick= {(e) => {
-                this.login()
-              }}/>
-            <FlatButton
-              label="Register"
-              onClick= {(e) => {
-                this.register()
-              }} />
-          </div>
-
-        </Paper>
-        <Dialog
-          actions={[
-            <FlatButton
-              label="Dismiss"
-              secondary={true}
-              onClick={this.closeAlert}
-            />
-          ]}
-          modal={false}
-          open={this.state.showAlert}
-          onRequestClose={this.closeAlert}>
-          {this.state.alertMessage}
-        </Dialog>
+      <div  >
+        <Form onSubmit={this.handleSubmit} className="login-form" style={styleObj} >
+          <FormItem>
+            {getFieldDecorator('userName', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            )}
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit" className="login-form-button" onClick= {(e) => {
+              this.login()
+            }} >
+              Log in
+            </Button>
+            <Button type="primary" htmlType="submit" className="login-form-button" onClick= {(e) => {
+              this.register()
+            }} >
+              Register
+            </Button>
+          </FormItem>
+        </Form>
       </div>
-    )
+    );
   }
 }
+
+const Auth = Form.create()(AuthForm);
 
 export {
   Auth
---
+++ microservices/ui/app/src/temp.js
@@ -0,0 +1,181 @@
+import React from 'react';
+import axios from 'axios';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+import LinearProgress from 'material-ui/LinearProgress';
+import {Card, CardText} from 'material-ui/Card';
+import { saveOffline, getSavedToken } from './config';
+import { authenticateUser } from './api';
+
+
+const textfieldStyle = {
+  width: 300
+}
+
+class Auth extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: '',
+      showLoadingIndicator: false,
+      showAlert: false,
+      alertMessage: ''
+    };
+  }
+
+  showProgressIndicator = (shouldShow) => {
+    this.setState({
+      ...this.state,
+      showLoadingIndicator: shouldShow
+    });
+  }
+
+  login = () => {
+    console.log('on login clicked');
+    this.showProgressIndicator(true)
+    authenticateUser(this.state.username, this.state.password, false).then(authResponse => {
+      this.showProgressIndicator(false)
+      console.log(authResponse);
+      if (authResponse.auth_token) {
+        //Save the auth token offline to be used by the filestore service
+        saveOffline(authResponse.auth_token);
+        //this.showAlert("Login Successful! \n Your auth credentials are: " + JSON.stringify(authResponse, null, 2));
+
+        window.location.assign("/home");
+      } else {
+        this.showAlert(JSON.stringify(authResponse));
+      }
+    });
+  }
+
+  register = () => {
+    console.log('on register clicked');
+    this.showProgressIndicator(true)
+    authenticateUser(this.state.username, this.state.password, true).then(authResponse => {
+      this.showProgressIndicator(false)
+      console.log(authResponse);
+      if (authResponse.auth_token) {
+        saveOffline(authResponse.auth_token)
+        //this.showAlert("SignUp Successful! \n Your auth credentials are: " + JSON.stringify(authResponse, null, 2));
+        this.showAlert("SignUp Successful! \n Please login.");
+
+
+      } else {
+        this.showAlert(JSON.stringify(authResponse));
+      }
+    });
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({
+      ...this.state,
+      username: e.target.value
+    });
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      ...this.state,
+      password: e.target.value
+    });
+  }
+
+  closeAlert = () => {
+    this.setState({
+      ...this.state,
+      showAlert: false,
+      alertMessage: ''
+    });
+  };
+
+  showAlert = (message) => {
+    this.setState({
+      ...this.state,
+      showAlert: true,
+      alertMessage: message
+    });
+  }
+
+
+  render() {
+    const styleObj = {
+      height: '350px',
+      width: '500px',
+      margin: '20px',
+      textAlign: 'center',
+      display: 'inline-block',
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%,-50%)'
+    };
+
+    const buttonContainerStyle = {
+      position: 'absolute',
+      bottom: '20px',
+      width: '100%',
+      textAlign: 'center'
+    };
+
+    return (
+      <div>
+        <Paper
+          style={styleObj}
+          zDepth={1}>
+          {this.state.showLoadingIndicator ? <LinearProgress mode="indeterminate" /> : null}
+          <br /><br />
+          <TextField
+            onChange={this.handleUsernameChange}
+            floatingLabelText="Username"
+            style={textfieldStyle}
+            hintText="Username"/>
+
+          <TextField
+            onChange={this.handlePasswordChange}
+            floatingLabelText="Password"
+            style={textfieldStyle}
+            type="password"
+            hintText="password"/>
+
+          <div style={buttonContainerStyle}>
+            <FlatButton
+              label="Login"
+              primary={true}
+              onClick= {(e) => {
+                this.login()
+              }}/>
+            <FlatButton
+              label="Register"
+              onClick= {(e) => {
+                this.register()
+              }} />
+          </div>
+
+        </Paper>
+        <Dialog
+          actions={[
+            <FlatButton
+              label="Dismiss"
+              secondary={true}
+              onClick={this.closeAlert}
+            />
+          ]}
+          modal={false}
+          open={this.state.showAlert}
+          onRequestClose={this.closeAlert}>
+          {this.state.alertMessage}
+        </Dialog>
+      </div>
+    )
+  }
+}
+
+export {
+  Auth
+};
