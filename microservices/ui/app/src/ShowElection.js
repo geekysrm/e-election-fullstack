d608a47647dc339571b650d8d8759bf6c028e602
Soumya <soumyacool2012@gmail.com>
2018-02-01 02:03:34 +0530

    New

--- microservices/ui/app/src/ShowElection.js
+++ microservices/ui/app/src/ShowElection.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Button } from 'antd';
 import 'antd/dist/antd.css';
 
-export default () => (
-    <h1>hi</h1>
+export default ( { match: { params: {id } } } ) => (
+    <h1>{id}</h1>
 );
