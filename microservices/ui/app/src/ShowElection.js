0f5573ad89986b993dff885c8b1b8ad7830752fe
Soumya <soumyacool2012@gmail.com>
2018-02-01 03:22:35 +0530

    Hellosrm

--- microservices/ui/app/src/ShowElection.js
+++ microservices/ui/app/src/ShowElection.js
@@ -17,32 +17,56 @@ class ShowElection extends Component {
         // };
     }
 
-    // componentDidMount() {
+
+    onViewNominations = () => {
+
+        axios({
+            method: 'post',
+            url: 'https://api.artfully11.hasura-app.io/get-nominations',
+            data: { eid: this.props.match.params.id },
+            config: { headers: { 'Content-Type': 'application/json' } }
+        })
+            .then(response => {
+                console.log(response.data);
+
+
+            })
+            .catch(error => {
+                alert(`Sorry, can't fetch nominations list right now!`);
+                console.log('Post request failed!');
+            });
+
+    }
+
+    // onVote = () => {
+
     //     axios({
     //         method: 'post',
-    //         url: 'https://api.artfully11.hasura-app.io/get-elections',
-
+    //         url: 'https://api.artfully11.hasura-app.io/vote',
+    //         data: {  },
     //         config: { headers: { 'Content-Type': 'application/json' } }
     //     })
     //         .then(response => {
     //             console.log(response.data);
 
 
-
-
-    //             this.setState({ elections: response.data });
     //         })
     //         .catch(error => {
-    //             alert(`Sorry, can't fetch elections right now!`);
+    //             alert(`Sorry, can't fetch nominations list right now!`);
     //             console.log('Post request failed!');
     //         });
+
     // }
+
 
 
     render() {
         return (
             <div>
-                <h1>{this.props.match.params.id}</h1>
+                <h1>Election with Id: {this.props.match.params.id}</h1>
+                <Button type="primary" onClick={this.onViewNominations}>View Nominations</Button>
+                <Button type="primary" onClick={this.onVote}>Vote</Button>
+                <Button type="primary" onClick={this.onNominate}>Nominate Yourself</Button>
 
             </div>
         );
