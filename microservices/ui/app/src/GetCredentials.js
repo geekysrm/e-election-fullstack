0472cb5ea4cd99e49a34927b4d93ee5dbad30472
sonu <sonusaikishan@gmail.com>
2018-02-04 02:23:17 +0530

    x

--- microservices/ui/app/src/GetCredentials.js
+++ microservices/ui/app/src/GetCredentials.js
@@ -570,9 +570,13 @@ class CredentialsForm extends React.Component {
                             Display get credentials
                             */
                         }
-                        {alertSpan}
-                        {copiedSpan}
                     </Form>
+                    <div style={styles.msg}>
+                      <div style={{width:'50%'}}>
+                      {alertSpan}
+                      {copiedSpan}
+                      </div>
+                    </div>
                 </div>)}
                 {(this.state.flag === 0) && (
                     <div>
@@ -623,6 +627,15 @@ class CredentialsForm extends React.Component {
             </div>
         );
     }
+}
+
+const styles={
+  msg:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
 }
 
 const GetCredentials = Form.create()(CredentialsForm);
