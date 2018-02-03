3167bb87f8d2f73155d6e9cb64392971c703e3c8
sonu <sonusaikishan@gmail.com>
2018-02-03 21:55:27 +0530

    x

--- microservices/ui/app/src/temp-ShowElection.js
+++ microservices/ui/app/src/temp-ShowElection.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Button, Input , Divider } from 'antd';
+import { Button, Input } from 'antd';
 import moment from 'moment';
 import 'antd/dist/antd.css';
 import { getSavedToken } from './config';
@@ -15,7 +15,6 @@ class ShowElection extends Component {
 
         this.state = {
             electionState: '',         //Store details of the particular election state in the page
-            electionPost: '',
             nominations: [],
             nominee_names: [],
             nominee_ages: [],
@@ -75,7 +74,7 @@ class ShowElection extends Component {
             .then(response => {
                 console.log(response.data[0].state);
                 this.setState({ electionState: response.data[0].state });
-                this.setState({ electionPost: response.data[0].post });
+
 
             })
             .catch(error => {
@@ -225,83 +224,47 @@ class ShowElection extends Component {
 
     render() {
         return (
-          <div>
+            <div>
+                <h1>Election with Id: {this.props.match.params.id}</h1>
 
-            <div style={styles.side}>
-              <h1 style={{color:'white' , fontFamily: 'Acme', fontSize: 60}}>E-Election</h1>
-              <nav style={{display:'block'}}>
-                <ul style={{listStyle:'none'}}>
-                  <li style={styles.link}><a className='a' href="/get-credentials">Get Credentials</a></li>
-                  <li style={styles.link}><a className='a' href="#" onClick={this.onLogout}>Logout</a></li>
-                  <li style={styles.link}><a className='a' href="#">About</a></li>
-                </ul>
-              </nav>
-            </div>
+                <Button type="primary" onClick={this.onNominate}>Nominate Yourself</Button>
+                <div>
 
-            <div style={styles.content}>
-                  <h1 style={{marginTop:"10px" , textAlign:"center"}}>{this.state.electionState} state {this.state.electionPost} elections</h1>
-                  <Divider />
-                  <Button type="primary" onClick={this.onNominate}>Nominate Yourself</Button>
-                  <div>
+                    <ol>
 
-                      <ol>
-
-                          {this.state.nominations.map((nomination, index) => {
-                              return (
+                        {this.state.nominations.map((nomination, index) => {
+                            return (
 
 
-                                  <li key={nomination.hasura_id}>
+                                <li key={nomination.hasura_id}>
 
-                                      <ul>
-                                          <li key={nomination.hasura_id}>
+                                    <ul>
+                                        <li key={nomination.hasura_id}>
 
-                                              <li key={this.state.nominee_names[index]}>Name: {this.state.nominee_names[index]}</li>
-                                              <li key={this.state.nominee_genders[index]}>Gender: {this.state.nominee_genders[index]}</li>
-                                              <li key={this.state.nominee_ages[index]}>Age: {this.state.nominee_ages[index]}</li>
-                                              {nomination.individual ? <li key={nomination.individual}>Party: Independent Candidate</li> : <li key={nomination.party}>Party: {nomination.party}</li>}
-                                              {!(nomination.individual) && <li key={nomination.party_ticket_id}>Party Ticket ID: {nomination.party_ticket_id}</li>}
-                                              <li key={nomination.manifesto}>Election Manifesto: {nomination.manifesto}</li>
-                                          </li>
-                                      </ul>
-                                      <Button type="primary" onClick={() => this.onVote(nomination.hasura_id, this.props.match.params.id)}>Vote</Button>
-                                      <br />
-                                      <br />
-                                      {this.state.textBoxShow === nomination.hasura_id && <Search placeholder="Enter your Voting Credentials"  enterButton="Cast Vote" onSearch={value => this.onCastVote(nomination.hasura_id, this.props.match.params.id, value)} />}
-                                      <br />
-                                      <br />
-                                  </li>
+                                            <li key={this.state.nominee_names[index]}>Name: {this.state.nominee_names[index]}</li>
+                                            <li key={this.state.nominee_genders[index]}>Gender: {this.state.nominee_genders[index]}</li>
+                                            <li key={this.state.nominee_ages[index]}>Age: {this.state.nominee_ages[index]}</li>
+                                            {nomination.individual ? <li key={nomination.individual}>Party: Independent Candidate</li> : <li key={nomination.party}>Party: {nomination.party}</li>}
+                                            {!(nomination.individual) && <li key={nomination.party_ticket_id}>Party Ticket ID: {nomination.party_ticket_id}</li>}
+                                            <li key={nomination.manifesto}>Election Manifesto: {nomination.manifesto}</li>
+                                        </li>
+                                    </ul>
+                                    <Button type="primary" onClick={() => this.onVote(nomination.hasura_id, this.props.match.params.id)}>Vote</Button>
+                                    <br />
+                                    <br />
+                                    {this.state.textBoxShow === nomination.hasura_id && <Search placeholder="Enter your Voting Credentials"  enterButton="Cast Vote" onSearch={value => this.onCastVote(nomination.hasura_id, this.props.match.params.id, value)} />}
+                                    <br />
+                                    <br />
+                                </li>
 
-                              );
-                          })}
-                      </ol>
-                  </div>
+                            );
+                        })}
+                    </ol>
+                </div>
 
             </div>
-
-            <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet" />
-          </div>
         );
     }
-}
-
-const styles={
-  side:{
-    float:'left',
-  	width:'30%',
-  	paddingTop:'30px',
-  	paddingLeft:'25px',
-  	top:0,
-  	bottom:0,
-    position:'fixed',
-  	backgroundColor:'#474958',
-  	zIndex:400
-  },
-  content:{
-    float:'left',
-  	marginLeft: '35%',
-  	width:'60%'
-  }
 }
 
 //implement this after clicking cast vote btn once: disabled={this.state.disableCastVoteButton}
