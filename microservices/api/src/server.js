77043b6f0c4425d3865cda726eefe8717a2315cd
sonu <sonusaikishan@gmail.com>
2018-02-07 18:47:44 +0530

    .

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -24,7 +24,30 @@ app.get('/',function(req,res){
 });
 
 app.get('/date',function(req,res){
-  res.send(Date());
+  var loc = '28.704059, 77.102490' // Tokyo expressed as lat,lng tuple
+  var targetDate = new Date() // Current date/time of user computer
+  var timestamp = targetDate.getTime()/1000 + targetDate.getTimezoneOffset() * 60 // Current UTC date/time expressed as seconds since midnight, January 1, 1970 UTC
+  var apikey = 'AIzaSyD4AlMeIBPVl4bNpLM1cgeaAwmJmAQf1iY'
+  var apicall = 'https://maps.googleapis.com/maps/api/timezone/json?location=' + loc + '&timestamp=' + timestamp + '&key=' + apikey
+
+  var xhr = new XMLHttpRequest() // create new XMLHttpRequest2 object
+  xhr.open('GET', apicall) // open GET request
+  xhr.onload = function(){
+      if (xhr.status === 200){ // if Ajax request successful
+          var output = JSON.parse(xhr.responseText) // convert returned JSON string to JSON object
+          console.log(output.status) // log API return status for debugging purposes
+          if (output.status == 'OK'){ // if API reports everything was returned successfully
+              var offsets = output.dstOffset * 1000 + output.rawOffset * 1000 // get DST and time zone offsets in milliseconds
+              var localdate = new Date(timestamp * 1000 + offsets) // Date object containing current time of Tokyo (timestamp + dstOffset + rawOffset)
+              console.log(localdate.toLocaleString()) // Display current Tokyo date and time
+              res.send(localdate.toLocaleString());
+          }
+      }
+      else{
+          alert('Request failed.  Returned status of ' + xhr.status)
+      }
+  }
+  xhr.send() // send request
 });
 
 app.get('/time',function(req,res){
