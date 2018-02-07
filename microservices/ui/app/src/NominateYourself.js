9395aa720b7d17ae7cf8c707dc35e2141f826f23
Soumya <soumyacool2012@gmail.com>
2018-02-08 03:39:09 +0530

    SRM

--- microservices/ui/app/src/NominateYourself.js
+++ microservices/ui/app/src/NominateYourself.js
@@ -306,8 +306,7 @@ class NominateYourselfForm extends React.Component {
                 
                 : 
                 <div>
-                <h1 style={{ marginTop: "10px", textAlign: "center" }}>Nomination</h1>
-                <Divider />
+                
                         <Alert style={{ marginTop: "10px", textAlign: "center", width: '80%', margin: "0 auto" }}
                         message="Sorry, you cannot nominate yourself for this post right now!"
                         description="The nomination process for this post is not going on right now."
